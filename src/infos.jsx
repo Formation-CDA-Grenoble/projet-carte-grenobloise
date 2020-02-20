dcb5c3c73a2412bbbecb64b1cb79c2ee60e34002
Naim391 <54673303+Naim391@users.noreply.github.com>
2020-02-20 10:30:58 +0100

    Suppression fichiers inutiles

--- src/infos.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const infos = (props)=>
-<div>
-    <h1> {props.mayorname} </h1>
-    <div> habitants number : {props.habitantsnumber}</div>
-    <div> area : {props.area}</div>
-    <div> tramlines : {props.tramlines}</div>
-</div>;
-export default infos;
